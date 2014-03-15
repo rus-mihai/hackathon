6f8f2326bea79eb52f2220fff715e60658ea92df
Alexandra Coldea <alexandra.coldea@isdc.eu>
2014-03-15 20:27:03 +0200

    Updated search query

--- ourWebapp/src/main/webapp/js/events.js
+++ ourWebapp/src/main/webapp/js/events.js
@@ -1,37 +1,40 @@
 (function($) {
 
-var eventHtml = "<li><a href='http://www.visitclujnapoca.ro' target='_blank'><img src='http://www.visitclujnapoca.ro/images/evenimente/{0}'><div>{1}</div></img></a></li>";
- 
- function loadFund() {
-	var query = {"query": {
-				         "query_string": {
-				            "query": "1",
-				            "fields": ["dateEvent"]
-				        }
-				    }
-				};
+	var eventHtml = "<li><a href='http://www.visitclujnapoca.ro' target='_blank'><img src='http://www.visitclujnapoca.ro/images/evenimente/{0}'><div>{1}</div></img></a></li>";
 
-  $.ajax({
-  	 type: 'POST',
-  	 data: JSON.stringify(query,""),
-  	 dataType: "json",
-     url : "http://localhost:9200/events/event/_search",
-     success : function(response) {
-       var values = response.hits.hits, 
-       		eventsBannerHtml = "";
-       $.each(values, function( index, value ) {
-       var currentEventHtml = eventHtml.replace("{0}", value._source.image);
-       		eventsBannerHtml += currentEventHtml.replace("{1}", value._source.identifier);
+	function loadFund() {
+
+		var query = {
+			"query" : {
+				"range" : {
+					"data_eveniment" : {
+						"gte" : "2014-03-6 0:00:00"
+					}
+				}
+			}
+		};
+
+		$.ajax({
+			type : 'POST',
+			data : JSON.stringify(query, ""),
+			dataType : "json",
+			url : "http://54.72.105.17:8080/events/event/_search",
+			success : function(response) {
+				var values = response.hits.hits, eventsBannerHtml = "";
+				$.each(values, function(index, value) {
+					var currentEventHtml = eventHtml.replace("{0}",
+							value._source.img);
+					eventsBannerHtml += currentEventHtml.replace("{1}",
+							value._source.identifier);
+				});
+
+				$("#carousel ul").html(eventsBannerHtml);
+			}
 		});
-		
-		$("#carousel ul").html(eventsBannerHtml);
-     }
-    });
- }
+	}
 
- $(function() {
-  loadFund();
-  
- 
- });
+	$(function() {
+		loadFund();
+
+	});
 })(jQuery);
